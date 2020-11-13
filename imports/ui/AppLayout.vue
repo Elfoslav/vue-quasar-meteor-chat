6fef23795dfe3cb0b6ed386507ed9835edd1ee49
elfoslav <tom.hromnik@gmail.com>
2020-11-13 16:59:35 +0100

    Fix AppLayout

--- imports/ui/AppLayout.vue
+++ imports/ui/AppLayout.vue
@@ -1,69 +1,66 @@
 <template name="appLayout">
+  <q-layout ref="layout" view="LHr lpR lFr">
+    <q-header elevated class="bg-primary text-white">
+      <q-toolbar>
+        <q-btn
+          flat round dense
+          @click="showLeft = !showLeft"
+          icon="menu"
+        />
 
+        <q-toolbar-title>
+          Chat app
+        </q-toolbar-title>
 
-    <q-layout ref="layout" view="LHr lpR lFr">
+        <q-btn
+          flat round dense
+          @click="showRight = !showRight"
+          icon="menu"
+        />
+      </q-toolbar>
+      <q-tabs>
+        <q-route-tab slot="title" icon="save" to="/" replace label="Home" />
+        <q-route-tab slot="title" icon="help" to="/info" replace label="Info" />
+      </q-tabs>
+    </q-header>
 
-        <q-header elevated class="bg-primary text-white">
-            <q-toolbar>
-                <q-btn
-                        flat round dense
-                        @click="showLeft = !showLeft"
-                        icon="menu"
-                />
+    <q-drawer side="left" v-model="showLeft">
+      <q-list no-border link inset-separator>
+        <q-item-label header>Essential Links</q-item-label>
+        <q-item clickable to="/">
+          <q-item-section avatar>
+            <q-icon color="primary" name="save" />
+          </q-item-section>
+          <q-item-section>Home</q-item-section>
+        </q-item>
+        <q-separator spaced inset="item" />
+        <q-item clickable to="/info">
+          <q-item-section avatar>
+            <q-icon color="primary" name="help" />
+          </q-item-section>
+          <q-item-section>Info</q-item-section>
+        </q-item>
+        <q-separator spaced inset="item" />
+      </q-list>
+    </q-drawer>
 
-                <q-toolbar-title>
-                    Layout Header
-                </q-toolbar-title>
+    <q-drawer side="right" v-model="showRight">
+      <span>Right Drawer</span>
+    </q-drawer>
 
-                <q-btn
-                        flat round dense
-                        @click="showRight = !showRight"
-                        icon="menu"
-                />
-            </q-toolbar>
-            <q-tabs>
-                <q-route-tab slot="title" icon="save" to="/" replace label="Home" />
-                <q-route-tab slot="title" icon="help" to="/info" replace label="Info" />
-            </q-tabs>
-        </q-header>
+    <q-page-container>
+      <router-view></router-view>
+    </q-page-container>
 
-        <q-drawer side="left" v-model="showLeft">
-            <q-list no-border link inset-separator>
-                <q-item-label header>Essential Links</q-item-label>
-                <q-item clickable to="/">
-                    <q-item-section avatar>
-                        <q-icon color="primary" name="save" />
-                    </q-item-section>
-                    <q-item-section>Home</q-item-section>
-                </q-item>
-                <q-separator spaced inset="item" />
-                <q-item clickable to="/info">
-                    <q-item-section avatar>
-                        <q-icon color="primary" name="help" />
-                    </q-item-section>
-                    <q-item-section>Info</q-item-section>
-                </q-item>
-                <q-separator spaced inset="item" />
-            </q-list>
-        </q-drawer>
+    <q-footer>
+      <q-toolbar>
+        <q-toolbar-title>
+          <span>Footer is here</span>
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-footer>
 
-        <q-drawer side="right" v-model="showRight">
-            <span>Right Drawer</span>
-        </q-drawer>
-
-        <q-page-container>
-            <router-view></router-view>
-        </q-page-container>
-
-        <q-footer>
-            <q-toolbar>
-                <q-toolbar-title>
-                    <span>Footer is here</span>
-                </q-toolbar-title>
-            </q-toolbar>
-        </q-footer>
-
-    </q-layout>
+  </q-layout>
 
 </template>
 
